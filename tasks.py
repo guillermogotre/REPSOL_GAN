a5ac40055da0a19db1a91fc79a2306671e3cd679
Guillermo <guille.eseh@gmail.com>
2019-11-15 11:21:57 +0100

    test

--- tasks.py
+++ tasks.py
@@ -3,7 +3,7 @@ import os
 import json
 
 @task
-def run(c, config_path='./config.json', git=False):
+def run(c, config_path='./config.json', git=False, update=True):
     assert (os.path.isfile(config_path))
     with open(config_path,'r') as ifile:
         config_json = json.load(ifile)
@@ -13,6 +13,9 @@ def run(c, config_path='./config.json', git=False):
 
     if git:
         c.run('git pull')
+
+    if update:
+        c.run('pip install -r requirements.txt --user')
 
     c.run("CUDA_VISIBLE_DEVICES={} python main.py".format(config_json['GPU_ID']))
 
