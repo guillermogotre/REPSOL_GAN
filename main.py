bc81d7eebfe061f8827d72251b270a1ff4014db0
Guillermo <guille.eseh@gmail.com>
2019-11-15 11:44:18 +0100

    test

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
 import tensorflow as tf
+
+import json
+
+import progressbar
+
+from tensorflow.keras import layers
+
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import ShuffleSplit
+
+import matplotlib.pyplot as plt
+import pickle
+import time
+
+BATCH_SIZE = 16
+# BUFFER_SIZE = 2**20
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dev', dest='dev', action='store_true')
+parser.add_argument('--config', dest='config_path', type=str, required=True)
+args = parser.parse_args()
+
+with open(args.config_path,'r') as ifile:
+    CONFIG_JSON = json.load(ifile)['development' if args.dev else 'production']
+
 if __name__ == "__main__":
     print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
     # Create some tensors
--- tasks.py
+++ tasks.py
@@ -3,10 +3,10 @@ import os
 import json
 
 @task
-def run(c, config_path='./config.json', git=False, update=True):
+def run(c, config_path='./config.json', git=False, update=True, dev=True):
     assert (os.path.isfile(config_path))
     with open(config_path,'r') as ifile:
-        config_json = json.load(ifile)
+        config_json = json.load(ifile)['development' if dev else 'production']
     for p in [config_json['INDATA_FOLDER'],config_json['OUTDATA_FOLDER']]:
         if not os.path.isdir(p):
             os.makedirs(p)
@@ -17,5 +17,5 @@ def run(c, config_path='./config.json', git=False, update=True):
     if update:
         c.run('pip install -r requirements.txt --user')
 
-    c.run("CUDA_VISIBLE_DEVICES={} python main.py".format(config_json['GPU_ID']))
+    c.run("CUDA_VISIBLE_DEVICES={} python main.py {} --config{}".format(config_json['GPU_ID'], "--dev" if dev else "", config_path))
 
