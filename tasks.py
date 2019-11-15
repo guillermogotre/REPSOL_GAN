168025908c06dbd072a3868a30289eb32e380dea
Guillermo <guille.eseh@gmail.com>
2019-11-15 11:08:15 +0100

    first commit

--- tasks.py
+++ tasks.py
@@ -1,1 +1,19 @@
 from invoke import task
+import os
+import json
+
+@task
+def run(c, config_path='./config.json', git=False):
+    print(os.listdir('.'))
+    assert (os.path.isfile(config_path))
+    with open(config_path,'r') as ifile:
+        config_json = json.load(ifile)
+    for p in [config_json['INDATA_FOLDER'],config_json['OUTDATA_FOLDER']]:
+        if not os.path.isdir(p):
+            os.makedirs(p)
+
+    if git:
+        c.run('git pull')
+
+    c.run('python main.py')
+
